ca6368958b20845b9503340c525418388eb1be2a
E.Azer Koçulu <azer@kodfabrik.com>
2012-07-22 12:56:55 -0700

    yasnippet & ezbl update

--- conf/yasnippet.el
+++ conf/yasnippet.el
@@ -1,2 +1,17 @@
-(yas/initialize)
+(add-lib-path "yasnippet")
+
+(require 'yasnippet)
 (yas/load-directory "~/.local/share/snippets")
+(setq yas/snippet-dirs '("~/.local/share/snippets"))
+(yas/global-mode 1)
+(setq yas/trigger-key "M-SPC")
+
+(eval-after-load 'js2-mode
+  '(progn
+     (define-key js2-mode-map (kbd "TAB") (lambda()
+                                            (interactive)
+                                            (let ((yas/fallback-behavior 'return-nil))
+                                              (unless (yas/expand)
+                                                (indent-for-tab-command)
+                                                (if (looking-back "^\s*")
+                                                    (back-to-indentation))))))))
--- profiles/dev.el
+++ profiles/dev.el
@@ -1,17 +1,18 @@
 (load-conf-file "slime.el")
 
-(require 'yasnippet)
+;;(load-conf-file "ezbl.el")
 
+(load-conf-file "js2.el")
 (load-conf-file "auto-complete.el")
 (load-conf-file "clojure.el")
 (load-conf-file "highlight-flash.el")
-(load-conf-file "js2.el")
 (load-conf-file "linum.el")
 (load-conf-file "lisp.el")
 (load-conf-file "smex.el")
 (load-conf-file "paredit.el")
-(load-conf-file "yasnippet.el")
 (load-conf-file "rust.el")
+
+(load-conf-file "yasnippet.el")
 ;;(load-conf-file "ecb.el")
 ;;(load-conf-file "coffee-script.el")
 
