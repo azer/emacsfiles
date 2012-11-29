dc05021879c763ca235a8df19ed6739cf434e250
E.Azer Koçulu <azer@kodfabrik.com>
2012-11-29 12:23:03 -0800

    add keybinding to set make params

--- conf/make.el
+++ conf/make.el
@@ -1,6 +1,19 @@
+(setq make-params "")
+
 (defun run-make-command ()
   "Run `make` command on working directory"
   (interactive)
-  (compile "make"))
+  (let ((cmd (concat "make " make-params)))
+    (message (concat "Running " cmd))
+    (compile cmd))
+  )
+
+(defun set-make-params (params)
+  "Set the parameters of make command"
+  (interactive "sEnter parameters for make:")
+  (message "Default make command set to 'make %s'" params)
+  (setq make-params params)
+)
 
 (global-set-key (kbd "M-m") 'run-make-command)
+(global-set-key (kbd "M-n") 'set-make-params)
